ee9f464e40c54948dbdcc481236b6f8155be9a14
Bipul2023 <106473306+Bipul2023@users.noreply.github.com>
2024-01-04 22:15:31 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gamil.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bipulhaldar73@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bipul@83%'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
